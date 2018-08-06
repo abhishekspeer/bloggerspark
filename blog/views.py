190a64f5c8b83f366ef2305048f2453208fbc51a
abhishekspeer <abhishek.abhishekgaur.gaur@gmail.com>
2018-08-06 15:48:47 +0530

    Added Blog Published Dates

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
+from django.shortcuts import redirect
 # Create your views here.
 '''
 A view is a place where we put the "logic" of our application. It will request
@@ -8,7 +10,7 @@ information from the model you created before and pass it to a template
 tells template how to use data from models
 '''
 def post_list(request):
-    posts = Post.objects.filter(publised_date__lte=timezone.now()).order_by('publised_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # posts <queryset>
     return render(request, 'blog/post_list.html', {'posts': posts})
     # {} stuff is used by the template
